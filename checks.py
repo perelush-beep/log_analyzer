1cd430920ec884765301a333da28b67965c63215
Aharom_Perel <perelush@gmail.com>
2026-02-10 15:11:24 +0200

    added filter by weight

--- checks.py
+++ checks.py
@@ -1,4 +1,10 @@
-from config import authorised_ips
+from config import *
 def external_ips(data:list[list[str]]):
     extracted_ip = [line[1] for line in data if not line[1].startswith(authorised_ips)]
     return extracted_ip
+def port_filter(data:list[list[str]]):
+    extracted_ports = [line for line in data if line[3].strip() in sensitive_ports]
+    return extracted_ports
+def weight_filter(data:list[list[str]]):
+    extracted_by_weight = [line for line in data if int(line[5].strip()) > heavy_file]
+    return extracted_by_weight
--- config.py
+++ config.py
@@ -1,1 +1,3 @@
-authorised_ips = ("10", "192.168")
+authorised_ips = ("10.", "192.168")
+sensitive_ports = ("22", "23", "3389")
+heavy_file = 5000
