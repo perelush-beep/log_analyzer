8cec818dcbd235572ea35fcc4b9a18580bb84dbf
Aharom_Perel <perelush@gmail.com>
2026-02-11 12:13:48 +0200

    added inquiries counter for source ips

--- reporter.py
+++ reporter.py
@@ -2,3 +2,5 @@ from config import heavy_file
 def weight_tags(data:list[list[str]]):
     extracted_by_weight = ["LARGE" if int(line[5].strip()) > heavy_file else "NORMAL" for line in data]
     return extracted_by_weight
+def inquiries(data:list[list[str]])->dict:
+    return {ip:data.count(ip) for ip in set(data)}
